9557242a01f6646ac6d1b0973c4b1e5951be73ea
Elisa Situ Huang <elisasituhuang@gmail.com>
2022-10-01 19:27:32 -0400

    commit tree ee2709ac8587026dbd255d6fd40560c7e345a8a6

--- index.js
+++ index.js
@@ -1,1 +1,32 @@
 // Write your solution in this file!
+const employee = {
+    name: "Elisa",
+    streetAddress: "11 Sunset ave",
+};
+function updateEmployeeWithKeyAndValue(employee, key, value) {
+   const newObj = { ...employee};
+
+  newObj[key] = value;
+
+  return newObj;
+}
+function destructivelyUpdateEmployeeWithKeyAndValue(employee, key, value) {
+    employee[key] = value;
+    return employee;
+}
+
+function deleteFromEmployeeByKey(employee, key) { 
+ 
+   const nObject = {...employee, ...key};
+   
+   nObject[key] = delete nObject['key'];
+    return nObject;
+}
+
+
+//function destructivelyDeleteFromEmployeeByKey(employee, key){
+   
+//delete destructivelyDeleteFromEmployeeByKey().key;
+  //  const nObject = {...employee};
+    // return nObject
+// 
